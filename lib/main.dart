a6fc2032caef19ae5ebdbc0460c547e470fa42e5
lordartemio02 <lordartemio02@gmail.com>
2021-11-01 21:19:10 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/models/client_model.dart';
+import 'package:flutter_application_1/database/data_base.dart';
+import 'dart:math' as math;
+
+void main() => runApp(const MaterialApp(home: MyApp()));
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // data for testing
+  List<Client> testClients = [
+    Client(firstName: "Raouf", lastName: "Rahiche", blocked: false),
+    Client(firstName: "Zaki", lastName: "oun", blocked: true),
+    Client(firstName: "oussama", lastName: "ali", blocked: false),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Flutter SQLite")),
+      body: FutureBuilder<List<Client>>(
+        future: DBProvider.db.getAllClients(),
+        builder: (BuildContext context, AsyncSnapshot<List<Client>> snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (BuildContext context, int index) {
+                Client item = snapshot.data![index];
+                return Dismissible(
+                  key: UniqueKey(),
+                  background: Container(color: Colors.red),
+                  onDismissed: (direction) {
+                    DBProvider.db.deleteClient(item.id!);
+                  },
+                  child: ListTile(
+                    title: Text(item.lastName!),
+                    leading: Text(item.id.toString()),
+                    trailing: Checkbox(
+                      onChanged: (bool? value) {
+                        DBProvider.db.blockOrUnblock(item);
+                        setState(() {});
+                      },
+                      value: item.blocked,
+                    ),
+                  ),
+                );
+              },
+            );
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () async {
+          Client rnd = testClients[math.Random().nextInt(testClients.length)];
+          await DBProvider.db.newClient(rnd);
+          setState(() {});
+        },
+      ),
+    );
+  }
+}
